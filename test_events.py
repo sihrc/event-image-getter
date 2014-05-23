9426bdcd6d5735978026b47a9429e5a9f3bfbcf7
Chris <sihrc.c.lee@gmail.com>
2014-05-22 18:08:23 -0700

    obtained test events from android application

---
+++ test_events.py
@@ -0,0 +1,92 @@
+from model import Event
+
+test_events = [
+	Event(name = "next to normal",description = "",location = "Elliot Program Center", date = "8:00 PM"),\
+	Event(name = "God Herself Could Not Sink This Ship",description = "",location = "The Nitery", date = "8:00 PM"),\
+	Event(name = "next to normal",description = "",location = "Elliot Program Center", date = "8:00 PM"),\
+	Event(name = "God Herself Could Not Sink This Ship",description = "",location = "The Nitery", date = "8:00 PM"),\
+	Event(name = "next to normal",description = "",location = "Elliot Program Center", date = "8:00 PM"),\
+	Event(name = "God Herself Could Not Sink This Ship",description = "",location = "The Nitery", date = "8:00 PM"),\
+	Event(name = "BUILD \"Meet the Brewer\"",description = "",location = "BUILD Pizzeria", date = "5:00 PM"),\
+	Event(name = "Flying TreeHouse Presents: The Big Bad Pie Fight and Other Stories",description = "",location = "Toyon Hall", date = "8:00 PM"),\
+	Event(name = "next to normal",description = "",location = "Elliot Program Center", date = "8:00 PM"),\
+	Event(name = "God Herself Could Not Sink This Ship",description = "",location = "The Nitery", date = "8:00 PM"),\
+	Event(name = "5/22 - TELEPORT + ROM @ Advance ++ Monarch SF",description = "",location = "Monarch", date = "9:00 PM"),\
+	Event(name = "next to normal",description = "",location = "Elliot Program Center", date = "8:00 PM"),\
+	Event(name = "God Herself Could Not Sink This Ship",description = "",location = "The Nitery", date = "8:00 PM"),\
+	Event(name = "Tiki Bar Happy Hour",description = "",location = "Truck", date = "4:00 PM"),\
+	Event(name = "Discussion: Two Years Before the Mast",description = "",location = "Golden Gate Yacht Club", date = "4:00 PM"),\
+	Event(name = "SFBayArea NPHC Chevy's",description = "",location = "Chevy's", date = "4:00 PM"),\
+	Event(name = "Introducing MyHavtorn - Organic Skincare from Sweden",description = "",location = "Ayla", date = "5:00 PM"),\
+	Event(name = "Lavender Graduation & Diversity Awards Ceremony, Reception and Queer Prom",description = "",location = "City College of San Francisco Queer Resource Center", date = "5:00 PM"),\
+	Event(name = "next to normal",description = "",location = "Elliot Program Center", date = "8:00 PM"),\
+	Event(name = "God Herself Could Not Sink This Ship",description = "",location = "The Nitery", date = "8:00 PM"),\
+	Event(name = "VINYL DREAMS' *Pisco Sour AMAZING MUSIC hour* THURSDAYS at MONARCH - COLE & TOURIST tonight!!!",description = "",location = "Monarch", date = "6:00 PM"),\
+	Event(name = "Technology's Role in Rebuilding the Political Center",description = "",location = "Wearable World", date = "6:00 PM"),\
+	Event(name = "Product Hunt Happy Hour",description = "",location = "Victory Hall & Parlor SF", date = "6:00 PM"),\
+	Event(name = "Bartending for a Great Cause!",description = "",location = "Royal Tug Yacht Club", date = "6:00 PM"),\
+	Event(name = "Fathoms: Kuze + Stereo Types + Rob Saizo",description = "",location = "Asiento", date = "6:00 PM"),\
+	Event(name = "SF Cutters: BlackMagicDesign Resolve PremiereProCC TrackXSliceX NAB UPDATES",description = "",location = "Adobe Systems Inc.", date = "6:30 PM"),\
+	Event(name = "Gumroad Creators Studio: Authentic Businesses in the Age of Online Noise with Leo Babauta",description = "",location = "Parisoma", date = "6:30 PM"),\
+	Event(name = "Black Flag",description = "",location = "Brick and Mortar Music Hall", date = "7:00 PM"),\
+	Event(name = "Seminar",description = "",location = "San Francisco Playhouse", date = "7:00 PM"),\
+	Event(name = "Beach Blanket Babylon",description = "",location = "Club Fugazi", date = "7:00 PM"),\
+	Event(name = "The Monroe Trio at Cafe Claude",description = "",location = "Cafe Claude", date = "7:30 PM"),\
+	Event(name = "Pearls Over Shanghai",description = "",location = "Hypnodrome", date = "8:00 PM"),\
+	Event(name = "TORQ presents Ummet Ozcan",description = "",location = "Ruby Skye", date = "8:00 PM"),\
+	Event(name = "Glass Menagerie",description = "",location = "The Costume Shop at ACT", date = "8:00 PM"),\
+	Event(name = "\"Best of PlayGround 18\" Festival",description = "",location = "Thick House", date = "8:00 PM"),\
+	Event(name = "8bitSF presents: Mega Ran, Dual Core & Big Al w/ DJ Mr Smith",description = "",location = "DNA Lounge", date = "8:00 PM"),\
+	Event(name = "William Shakespeare's ROMEO AND JULIET * NINJAZ OF DRAMA",description = "",location = "Phoenix Theatre", date = "8:00 PM"),\
+	Event(name = "The Homosexuals",description = "",location = "New Conservatory Theatre Center", date = "8:00 PM"),\
+	Event(name = "Black Flag",description = "",location = "Brick & Mortar Music Hall", date = "8:00 PM"),\
+	Event(name = "San Francisco Symphony - MTT and Yuja Wang",description = "",location = "Davies Symphony Hall", date = "8:00 PM"),\
+	Event(name = "Astonishment! Magical Tales of San Francisco",description = "",location = "Chancellor Hotel", date = "8:00 PM"),\
+	Event(name = "The Write Stuff",description = "",location = "Tides Theatre", date = "8:00 PM"),\
+	Event(name = "Dracula",description = "",location = "Shelton Theater", date = "8:00 PM"),\
+	Event(name = "Collected Stories",description = "",location = "Cartwright Hotel on Union Square", date = "8:00 PM"),\
+	Event(name = "next to normal",description = "",location = "Elliot Program Center", date = "8:00 PM"),\
+	Event(name = "God Herself Could Not Sink This Ship",description = "",location = "The Nitery", date = "8:00 PM"),\
+	Event(name = "next to normal",description = "",location = "Elliot Program Center", date = "8:00 PM"),\
+	Event(name = "God Herself Could Not Sink This Ship",description = "",location = "The Nitery", date = "8:00 PM"),\
+	Event(name = "Beyond Words: The Art of Communication",description = "",location = "Oddball Film+Video", date = "8:00 PM"),\
+	Event(name = "Comedian Robert Hawkins",description = "",location = "Punch Line Comedy Club", date = "8:00 PM"),\
+	Event(name = "Band of Skulls",description = "",location = "The Regency Ballroom", date = "8:00 PM"),\
+	Event(name = "\"The Mission Position\": Live Stand-up Comedy",description = "",location = "Lost Weekend Video", date = "8:00 PM"),\
+	Event(name = "The Crucible",description = "",location = "Gough Street Playhouse", date = "8:00 PM"),\
+	Event(name = "Beach Blanket Babylon",description = "",location = "Club Fugazi", date = "8:00 PM"),\
+	Event(name = "Panda Bear",description = "",location = "The Fillmore", date = "8:00 PM"),\
+	Event(name = "William Fitzsimmons",description = "",location = "The Independent", date = "8:00 PM"),\
+	Event(name = "Comedian Yannis Pappas",description = "",location = "Cobb's Comedy Club", date = "8:00 PM"),\
+	Event(name = "Salt & Sage, Bonnie Sun",description = "",location = "The Lost Church", date = "8:15 PM"),\
+	Event(name = "blasthaus presents MICHAEL MAYER (kompakt)",description = "",location = "MERCER", date = "9:00 PM"),\
+	Event(name = "next to normal",description = "",location = "Elliot Program Center", date = "8:00 PM"),\
+	Event(name = "God Herself Could Not Sink This Ship",description = "",location = "The Nitery", date = "8:00 PM"),\
+	Event(name = "next to normal",description = "",location = "Elliot Program Center", date = "8:00 PM"),\
+	Event(name = "God Herself Could Not Sink This Ship",description = "",location = "The Nitery", date = "8:00 PM"),\
+	Event(name = "Midnight Thieves",description = "",location = "Hotel Utah", date = "9:00 PM"),\
+	Event(name = "This Thursday May 22nd | BLITZ n GLITZ  Party at Starlight Room | DJ's D-Sharp & West Kraven",description = "",location = "Starlight Room", date = "9:00 PM"),\
+	Event(name = "This Thursday May 22nd | BLITZ n GLITZ  Party at Starlight Room | DJ's D-Sharp & West Kraven",description = "",location = "Starlight Room", date = "9:00 PM"),\
+	Event(name = "**RESCHEDULED**5/1 - FLACO @ Advance DNB ++ at Monarch",description = "",location = "Advance DNB", date = "9:00 PM"),\
+	Event(name = "This Thursday May 22nd | BLITZ n GLITZ  Party at Starlight Room | DJ's D-Sharp & West Kraven",description = "",location = "Starlight Room", date = "9:00 PM"),\
+	Event(name = "RYZE with TASTER PETER (Tronic, Trapez, Truesol, Octopus)",description = "",location = "Harlot San Francisco", date = "9:00 PM"),\
+	Event(name = "AFROLICOUS SF CARNAVAL 2014 WARMUP PARTY",description = "",location = "Elbo Room", date = "9:00 PM"),\
+	Event(name = "Bayonics w/ Rojai & Mars Today - presented by GKR & SPARC",description = "",location = "SPARC", date = "9:00 PM"),\
+	Event(name = "Marrakech Magic Theater",description = "",location = "Marrakech Magic Theater", date = "9:00 PM"),\
+	Event(name = "Ummet Ozcan",description = "",location = "Ruby Skye Nightclub", date = "9:00 PM"),\
+	Event(name = "Wake Owl",description = "",location = "The Chapel San Francisco", date = "9:00 PM"),\
+	Event(name = "CITY ROOTS @ EL TORO",description = "",location = "El Toro Night Club", date = "9:00 PM"),\
+	Event(name = "The Crib | CLOSING PARTY | LAST PARTY EVER!",description = "",location = "City Nights", date = "9:30 PM"),\
+	Event(name = "I LOVE THURSDAYS featuring Haze Nightclub Resident & San Francisco's Top Talent DJ E-ROCK @ Infusion Lounge | RSVP for FREE List",description = "",location = "Infusion Lounge", date = "9:30 PM"),\
+	Event(name = "Biscuits & Blues",description = "",location = "Biscuits & Blues", date = "9:30 PM"),\
+	Event(name = "Dance like it's 2004 @ popscene",description = "",location = "POPSCENE", date = "9:30 PM"),\
+	Event(name = "2NITE!!!  RITUAL 4 YEAR ANNIVERSARY: THE OTHERS, MATTY G, PERKULATOR AND MORE! (2 ROOMS TIL 3AM)",description = "",location = "F8 1192 Folsom", date = "10:00 PM"),\
+	Event(name = "Slide, Bump Heavy & Dolce Present \"DELTA HEAVY\"",description = "",location = "Slide SF", date = "10:00 PM"),\
+	Event(name = "Touch Of Class Records at Vessel",description = "",location = "Vessel San Francisco", date = "10:00 PM"),\
+	Event(name = "Yoshi's San Francisco",description = "",location = "Yoshi's San Francisco", date = "10:00 PM"),\
+	Event(name = "I WANNA ROCK!!! @ The Monster Show",description = "",location = "The Edge", date = "10:15 PM"),\
+	Event(name = "next to normal",description = "",location = "Elliot Program Center", date = "8:00 PM"),\
+	Event(name = "God Herself Could Not Sink This Ship",description = "",location = "The Nitery", date = "8:00 PM"),\
+	Event(name = "next to normal",description = "",location = "Elliot Program Center", date = "8:00 PM"),\
+	Event(name = "God Herself Could Not Sink This Ship",description = "",location = "The Nitery", date = "8:00 PM")
+]
